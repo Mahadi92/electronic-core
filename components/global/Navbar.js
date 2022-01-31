035599e124a85a623b96240c99069005c45f143b
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2022-01-31 19:31:03 +0600

    navbar

--- components/global/Navbar.js
+++ components/global/Navbar.js
@@ -5,16 +5,11 @@ import { FiChevronDown } from "react-icons/fi";
 import Image from 'next/image';
 import Link from "next/link";
 import navbarData from '../../data/global/navbarData.json';
-import { useRouter } from 'next/router';
-;
 
 
 
 
 const Navbar = () => {
-
-    const [language, setLanguage] = useState("EN")
-
 
     return (
         <div className={style.nav_container}>
@@ -22,22 +17,18 @@ const Navbar = () => {
             {/* Top black navbar */}
             <div className={style.top_nav}>
                 <div className={style.top_nav_container}>
-                    <div>
-                        <div>
-                            <select name="language" id="topNavLang">
-                                <option value="en">EN</option>
-                                <option value="EN2">EN2</option>
-                                <option value="EN3">EN3</option>
-                                <option value="EN4">EN4</option>
-                            </select>
-                        </div>
-                        <div>
-                            <select name="currency" id="topNavCurrency">
-                                <option value="usd">usd</option>
-                                <option value="gbp">gbp</option>
-                                <option value="eur">eur</option>
-                            </select>
-                        </div>
+                    <div className={style.top_nav_welcome}>
+                        <select name="language" id="topNavLang">
+                            <option value="en">EN</option>
+                            <option value="EN2">EN2</option>
+                            <option value="EN3">EN3</option>
+                            <option value="EN4">EN4</option>
+                        </select>
+                        <select name="currency" id="topNavCurrency">
+                            <option value="usd">usd</option>
+                            <option value="gbp">gbp</option>
+                            <option value="eur">eur</option>
+                        </select>
                         <p>Welcome to Complex store!</p>
                     </div>
                     <div className={style.top_nav_action_container}>
@@ -54,24 +45,30 @@ const Navbar = () => {
                 <div className={style.search_container_img}>
                     <Image src="/static/logo.png" alt="logo" width="120" height="80" />
                 </div>
+
                 <form className={style.search_container_form}>
-                    <select>
-                        <option value="opt1">All categories</option>
-                        <option value="opt2">Option 2</option>
-                        <option value="opt3">Option 3</option>
-                        <option value="opt4">Option 4</option>
-                        <option value="opt5">Option 5</option>
-                    </select>
-                    <input type="search" placeholder="Search here..." />
-                    <button type="submit">Search</button>
+                    <div className={style.search_container_searchBox_select}>
+                        <select>
+                            <option value="opt1">All categories</option>
+                            <option value="opt2">Option 2</option>
+                            <option value="opt3">Option 3</option>
+                            <option value="opt4">Option 4</option>
+                            <option value="opt5">Option 5</option>
+                        </select>
+                    </div>
+                    <div className={style.search_container_searchBox_search}>
+                        <input type="search" placeholder="Search here..." />
+                        <button type="submit">Search</button>
+                    </div>
                 </form>
 
                 <div className={style.search_container_cartIco}>
-                    <FaShoppingCart />
-                    <span>0</span>
+                    <button>
+                        <FaShoppingCart />
+                        <span>0</span>
+                    </button>
                     <p>Your Cart</p>
                 </div>
-
             </div>
 
             {/* Nav Items */}
@@ -85,7 +82,6 @@ const Navbar = () => {
 
 
 const Nav = () => {
-    const location = useRouter()
 
     return (
         <nav className={style.navbar}>
@@ -98,53 +94,45 @@ const Nav = () => {
                     <option value="">items5</option>
                 </select>
             </div>
-            <div>
+            <div className={style.nav_items_container}>
                 {navbarData.map((link, i) => {
-
-                    let activeLink = false;
-                    if (location.pathname !== "/") {
-                        activeLink = link.linkDropdown.some(data => data.linkAddress.includes(location.pathname))
-                    } else {
-                        activeLink = false;
-                    }
-
                     return (
-                        <div key={i} className={style.dropdown}>
-                            {link.linkDropdown === "" ? (
-                                <Link href={link.linkAddress} className={style.dropbtn}>
-                                    {link.linkName}
+                        <div key={i} className={style.nav_items}>
+                            {!link.linkDropdown.length ? (
+                                <Link href={link.linkAddress}>
+                                    <a className={style.nav_item}>
+                                        {link.linkName}
+                                    </a>
                                 </Link>
                             ) : (
                                 <div>
-                                    {/* {console.log(location.pathname, link.linkAddress)} */}
                                     <>
                                         <Link href={link.linkAddress}>
-                                            <a className={location.pathname === link.linkAddress || activeLink ? style.activeDropBtn : style.dropbtn}>
+                                            <a className={style.nav_item}>
                                                 {link.linkName}
-                                                {link.linkDropdown.length > 0 &&
-                                                    <FiChevronDown />
-                                                }
+                                                <FiChevronDown />
                                             </a>
                                         </Link>
+                                        <div className={style.nav_dropdown_container}>
+                                            <div className={style.dropdown_parent}>
+                                                {link.linkDropdown.length !== 0 &&
+                                                    link.linkDropdown.map((data, i) => {
+                                                        return (
+                                                            <div key={i} className="">
+                                                                <Link href={data.linkAddress}>
+                                                                    <a className={style.drop_nav_item}>
+                                                                        {data.linkName}
+                                                                    </a>
+                                                                </Link>
+                                                            </div>
+                                                        );
+                                                    })}
+                                            </div>
+                                        </div>
                                     </>
                                 </div>
                             )
                             }
-                            <div className={style.dropdown_content}>
-                                <div className={style.invisibleMargin}></div>
-                                <div className={style.dropdown_parent}>
-                                    {link.linkDropdown.length !== 0 &&
-                                        link.linkDropdown.map((data, i) => {
-                                            return (
-                                                <div key={i} className="">
-                                                    <Link href={data.linkAddress}>
-                                                        {data.linkName}
-                                                    </Link>
-                                                </div>
-                                            );
-                                        })}
-                                </div>
-                            </div>
                         </div>
                     );
                 })}
